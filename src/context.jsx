789b82f24cbb9062f28caf5096ca0d9fe03e430c
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-09 15:19:42 +0200

    update

--- src/context.jsx
+++ src/context.jsx
@@ -9,13 +9,3 @@ export const AppProvider = ({ children }) => {
 
  
  export const useGlobalContext = () => useContext(App);
-  import { createContext, useContext, useState, useEffect } from 'react'
-
-  const AppContext = createContext()
-
-  export const AppProvider = ({ children }) => {
-    return <AppProvider.Provider value={{}}></AppProvider.Provider>
-  }
-
-  export const useGlobalContext = () => useContext(App)
- 
